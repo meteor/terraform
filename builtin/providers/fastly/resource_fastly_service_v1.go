db3271f44648e26f0efa29afd11e13be64086f5d
clint shryock <clint@ctshryock.com>
2016-03-17 13:50:24 -0500

    updating service names

--- builtin/providers/fastly/resource_fastly_service_v1.go
+++ builtin/providers/fastly/resource_fastly_service_v1.go
@@ -1,8 +1,11 @@
 package fastly
 
 import (
-	"github.com/hashicorp/terraform/helper/resource"
+	"fmt"
+	"log"
+
 	"github.com/hashicorp/terraform/helper/schema"
+	gofastly "github.com/sethvargo/go-fastly"
 )
 
 func resourceServiceV1() *schema.Resource {
@@ -17,23 +20,87 @@ func resourceServiceV1() *schema.Resource {
 				Type:     schema.TypeString,
 				Required: true,
 			},
+			"ActiveVersion": &schema.Schema{
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
 		},
 	}
 }
 
-func resourceServiceV1Create(d *schema.ResourceData, m interface{}) error {
-	d.SetId(resource.UniqueId())
+func resourceServiceV1Create(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*FastlyClient).conn
+
+	s, err := conn.CreateService(&gofastly.CreateServiceInput{
+		Name:    d.Get("name").(string),
+		Comment: "Created by Terraform",
+	})
+
+	if err != nil {
+		return err
+	}
+
+	d.SetId(s.ID)
+
+	return resourceServiceV1Read(d, meta)
+}
+
+func resourceServiceV1Read(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*FastlyClient).conn
+
+	s, err := conn.GetService(&gofastly.GetServiceInput{
+		ID: d.Id(),
+	})
+
+	if err != nil {
+		return err
+	}
+
+	log.Printf("\n---\nService Details: %#v\n---\n", s)
+
+	d.Set("name", s.Name)
+	d.Set("ActiveVersion", s.ActiveVersion)
+
 	return nil
 }
 
-func resourceServiceV1Read(d *schema.ResourceData, m interface{}) error {
-	return nil
+func resourceServiceV1Update(d *schema.ResourceData, meta interface{}) error {
+	if d.HasChange("name") {
+		conn := meta.(*FastlyClient).conn
+		_, err := conn.UpdateService(&gofastly.UpdateServiceInput{
+			ID:   d.Id(),
+			Name: d.Get("name").(string),
+		})
+
+		if err != nil {
+			return err
+		}
+	}
+	return resourceServiceV1Read(d, meta)
 }
 
-func resourceServiceV1Update(d *schema.ResourceData, m interface{}) error {
-	return nil
-}
+func resourceServiceV1Delete(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*FastlyClient).conn
 
-func resourceServiceV1Delete(d *schema.ResourceData, m interface{}) error {
+	err := conn.DeleteService(&gofastly.DeleteServiceInput{
+		ID: d.Id(),
+	})
+
+	if err != nil {
+		return err
+	}
+
+	l, err := conn.ListServices(&gofastly.ListServicesInput{})
+	if err != nil {
+		return fmt.Errorf("[WARN] Error listing servcies when deleting Fastly Service (%s): %s", d.Id(), err)
+	}
+
+	for _, s := range l {
+		if s.ID == d.Id() {
+			// service still found
+			return fmt.Errorf("[WARN] Tried deleting Service (%s), but was still found", d.Id())
+		}
+	}
+	d.SetId("")
 	return nil
 }
--- builtin/providers/fastly/resource_fastly_service_v1_test.go
+++ builtin/providers/fastly/resource_fastly_service_v1_test.go
@@ -5,32 +5,49 @@ import (
 	"log"
 	"testing"
 
+	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
 	gofastly "github.com/sethvargo/go-fastly"
 )
 
 func TestAccFastlyServiceV1_basic(t *testing.T) {
-	var service gofastly.ServiceDetail
+	var service gofastly.Service
+	name := acctest.RandString(10)
+	nameUpdate := acctest.RandString(10)
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
 		CheckDestroy: testAccCheckServiceV1Destroy,
 		Steps: []resource.TestStep{
 			resource.TestStep{
-				Config: testAccServiceV1Config,
+				Config: testAccServiceV1Config(name),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckServiceV1Exists("fastly_service_v1.foo", &service),
-					testAccCheckFastlyServiceV1Attributes(&service),
+					testAccCheckFastlyServiceV1Attributes(&service, name),
 					resource.TestCheckResourceAttr(
-						"fastly_service_v1.foo", "name", "testservice"),
+						"fastly_service_v1.foo", "name", name),
+					resource.TestCheckResourceAttr(
+						"fastly_service_v1.foo", "ActiveVersion", "0"),
+				),
+			},
+
+			resource.TestStep{
+				Config: testAccServiceV1Config(nameUpdate),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckServiceV1Exists("fastly_service_v1.foo", &service),
+					testAccCheckFastlyServiceV1Attributes(&service, nameUpdate),
+					resource.TestCheckResourceAttr(
+						"fastly_service_v1.foo", "name", nameUpdate),
+					resource.TestCheckResourceAttr(
+						"fastly_service_v1.foo", "ActiveVersion", "0"),
 				),
 			},
 		},
 	})
 }
 
-func testAccCheckServiceV1Exists(n string, service *gofastly.ServiceDetail) resource.TestCheckFunc {
+func testAccCheckServiceV1Exists(n string, service *gofastly.Service) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		rs, ok := s.RootModule().Resources[n]
 		if !ok {
@@ -41,16 +58,12 @@ func testAccCheckServiceV1Exists(n string, service *gofastly.ServiceDetail) reso
 			return fmt.Errorf("No Service ID is set")
 		}
 
-		log.Printf("\n---\nDEBUG\n---\nFound: %s\n---\n", rs.Primary.ID)
-		log.Printf("\n---\nWhat is meta: %#v\n\n---\n", testAccProvider.Meta())
 		conn := testAccProvider.Meta().(*FastlyClient).conn
-
 		latest, err := conn.GetService(&gofastly.GetServiceInput{
 			ID: rs.Primary.ID,
 		})
 
 		if err != nil {
-			log.Printf("\n---\nError: %s\n---\n", err)
 			return err
 		}
 
@@ -60,11 +73,11 @@ func testAccCheckServiceV1Exists(n string, service *gofastly.ServiceDetail) reso
 	}
 }
 
-func testAccCheckFastlyServiceV1Attributes(service *gofastly.ServiceDetail) resource.TestCheckFunc {
+func testAccCheckFastlyServiceV1Attributes(service *gofastly.Service, name string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 
-		if service.Name != "testservice" {
-			return fmt.Errorf("Bad name: %s", service.Name)
+		if service.Name != name {
+			return fmt.Errorf("Bad name, expected (%s), got (%s)", name, service.Name)
 		}
 
 		return nil
@@ -78,16 +91,28 @@ func testAccCheckServiceV1Destroy(s *terraform.State) error {
 		if rs.Type != "fastly_service_v1" {
 			continue
 		}
+		log.Printf("\n---\nDEBUG in delete for fastly service\n---\n")
 
-		// Try to find the Service
+		conn := testAccProvider.Meta().(*FastlyClient).conn
 
-		return nil
+		l, err := conn.ListServices(&gofastly.ListServicesInput{})
+		if err != nil {
+			return fmt.Errorf("[WARN] Error listing servcies when deleting Fastly Service (%s): %s", rs.Primary.ID, err)
+		}
+
+		for _, s := range l {
+			if s.ID == rs.Primary.ID {
+				// service still found
+				return fmt.Errorf("[WARN] Tried deleting Service (%s), but was still found", rs.Primary.ID)
+			}
+		}
 	}
-	return fmt.Errorf("No Service found error")
+	return nil
 }
 
-const testAccServiceV1Config = `
+func testAccServiceV1Config(name string) string {
+	return fmt.Sprintf(`
 resource "fastly_service_v1" "foo" {
-        name = "testservice"
+        name = "%s"
+}`, name)
 }
-`
